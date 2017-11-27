5d8e7b0a27ec4f1d82e0dab53b871f915c56d0fb
João Paulo do N. Freitas <jpgjoaopaulo95@hotmail.com>
2017-11-27 14:00:17 -0300

    Projeto OK!

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trabalhando de forma otimizada com display flex | ITEM 01</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<section class="flex">
+    <article class="flex-item">
+        <h1>Trabalhando de forma otimizada com display flex</h1>
+        <p>Lorem Ipsum Lorem Ipsum</p>
+    </article>
+    <article class="flex-item">
+        <h1>Trabalhando de forma otimizada com display flex</h1>
+        <p>Lorem Ipsum Lorem Ipsum</p>
+    </article>
+    <article class="flex-item">
+        <h1>Trabalhando de forma otimizada com display flex</h1>
+        <p>Lorem Ipsum Lorem Ipsum</p>
+    </article>
+    <article class="flex-item">
+        <h1>Trabalhando de forma otimizada com display flex</h1>
+        <p>Lorem Ipsum Lorem Ipsum</p>
+    </article>
+    <article class="flex-item">
+        <h1>Trabalhando de forma otimizada com display flex</h1>
+        <p>Lorem Ipsum Lorem Ipsum</p>
+    </article>
+</section>
+
+
+
+</body>
+</html>
